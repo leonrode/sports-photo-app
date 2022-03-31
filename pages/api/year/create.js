ee857c43727675c4602a73ba0b2072237692c31b
leonrode <leon.rode13@gmail.com>
2022-03-30 23:32:14 -0400

    💥 commented out /year/create due to imgur not implemented

--- pages/api/year/create.js
+++ pages/api/year/create.js
@@ -4,61 +4,55 @@ import formidable from "formidable";
 import fs from "fs";
 import { ObjectId } from "mongodb";
 
-import { ImgurClient } from "imgur";
-
 const handler = async (req, res) => {
-  if (req.method === "POST") {
-    const form = new formidable.IncomingForm({
-      keepExtensions: true,
-      multiples: true,
-    });
+  // if (req.method === "POST") {
+  //   const form = new formidable.IncomingForm({
+  //     keepExtensions: true,
+  //     multiples: true,
+  //   });
 
-    const imgurClient = new ImgurClient({
-      clientId: process.env.IMGUR_CLIENT_ID,
-    });
+  //   return form.parse(req, async (err, fields, files) => {
+  //     if (err != null) {
+  //       console.error(err);
+  //       return res.status(500).send();
+  //     }
 
-    return form.parse(req, async (err, fields, files) => {
-      if (err != null) {
-        console.error(err);
-        return res.status(500).send();
-      }
+  //     const id = new ObjectId();
+  //     const entry = {
+  //       ...fields,
+  //       cover: {},
+  //       _id: id,
+  //       events: [],
+  //     };
 
-      const id = new ObjectId();
-      const entry = {
-        ...fields,
-        cover: {},
-        _id: id,
-        events: [],
-      };
+  //     try {
+  //       const imgurResponse = await imgurClient.upload({
+  //         image: fs.createReadStream(files.file.filepath),
+  //         type: "stream",
+  //       });
 
-      try {
-        const imgurResponse = await imgurClient.upload({
-          image: fs.createReadStream(files.file.filepath),
-          type: "stream",
-        });
+  //       entry.cover = {
+  //         link: imgurResponse.data.link,
+  //         hash: imgurResponse.data.deletehash,
+  //         datetime: imgurResponse.data.datetime,
+  //       };
+  //     } catch (e) {
+  //       return res.status(500).send();
+  //     }
 
-        entry.cover = {
-          link: imgurResponse.data.link,
-          hash: imgurResponse.data.deletehash,
-          datetime: imgurResponse.data.datetime,
-        };
-      } catch (e) {
-        return res.status(500).send();
-      }
-
-      // create event in database
-      const client = await clientPromise;
-      const db = client.db("data");
-      try {
-        const insertResult = db.collection("years").insertOne(entry);
-      } catch (e) {
-        console.error(e);
-        return res.status(500).send();
-      }
-      delete entry._id;
-      return res.json({ entry: entry });
-    });
-  }
+  //     // create event in database
+  //     const client = await clientPromise;
+  //     const db = client.db("data");
+  //     try {
+  //       const insertResult = db.collection("years").insertOne(entry);
+  //     } catch (e) {
+  //       console.error(e);
+  //       return res.status(500).send();
+  //     }
+  //     delete entry._id;
+  //     return res.json({ entry: entry });
+  //   });
+  // }
   return res.status(404).send();
 };
 

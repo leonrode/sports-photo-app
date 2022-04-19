f3724de300f482b3cc46674b2f55d22a69ed6a92
leonrode <leon.rode13@gmail.com>
2022-04-19 13:26:41 -0400

    wait until image load to show event to minimize layout shift

--- components/Event.jsx
+++ components/Event.jsx
@@ -4,13 +4,21 @@ import { parseDate } from "../lib/utils";
 
 import Link from "next/link";
 
+import { useState } from "react";
+
 const Event = ({ event }) => {
+  const [loaded, setLoaded] = useState(false);
   return (
     <Link href={`/event/${event.slug}`}>
-      <div className="group relative h-fit w-fit cursor-pointer">
+      <div
+        className={`${
+          loaded ? "block" : "hidden"
+        } group relative h-fit w-fit cursor-pointer`}
+      >
         <img
           className="rounded-xl "
           src={event.images[0] ? event.images[0].smaller : "/placeholder.png"}
+          onLoad={() => setLoaded(true)}
         ></img>
         <div className="py-2 flex justify-between items-center px-4 absolute bottom-0 rounded-br-lg rounded-bl-lg w-full bg-white">
           <div>

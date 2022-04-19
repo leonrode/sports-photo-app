b98cf00df42f803a83202e026f1c023491b95bff
leonrode <leon.rode13@gmail.com>
2022-04-19 13:29:06 -0400

    wait for year folder image to load before showing

--- components/YearFolder.jsx
+++ components/YearFolder.jsx
@@ -1,11 +1,22 @@
 import { FiChevronRight } from "react-icons/fi";
 import Link from "next/link";
+
+import { useState } from "react";
 const YearFolder = ({ year }) => {
+  const [loaded, setLoaded] = useState(false);
 
   return (
     <Link href={`/year/${year.year}`}>
-      <div className="group relative h-fit w-fit cursor-pointer">
-        <img className="rounded-xl" src={year.cover.smaller ? year.cover.smaller : "/placeholder.png"}></img>
+      <div
+        className={`${
+          loaded ? "block" : "hidden"
+        } group relative h-fit w-fit cursor-pointer`}
+      >
+        <img
+          className="rounded-xl"
+          src={year.cover.smaller ? year.cover.smaller : "/placeholder.png"}
+          onLoad={() => setLoaded(true)}
+        ></img>
         <div className="py-2 flex justify-between items-center px-4 absolute bottom-0 rounded-br-lg rounded-bl-lg w-full bg-white">
           <div>
             <h1 className="text-xl font-bold">{year.year}</h1>

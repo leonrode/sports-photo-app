c4e34fa06e801293c876edec1bee9a24cc9fcbfb
leonrode <leon.rode13@gmail.com>
2022-05-03 12:22:15 -0400

    mobile portrait issues

--- components/FullscreenImage.jsx
+++ components/FullscreenImage.jsx
@@ -8,15 +8,15 @@ import {
   FiChevronRight,
 } from "react-icons/fi";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { parseDate } from "../lib/utils";
 
 const FullscreenImage = ({ event, link, smallerLink, toClose, imageIndex }) => {
   const [copied, setCopied] = useState(false);
-
+  const imgRef = useRef(null);
   const [viewingImageIndex, setViewingImageIndex] = useState(imageIndex);
-
+  const [imgDimensions, setImgDimensions] = useState(null);
   useEffect(() => {
     const listener = (e) => {
       if (e.key === "ArrowRight") {
@@ -34,10 +34,17 @@ const FullscreenImage = ({ event, link, smallerLink, toClose, imageIndex }) => {
   }, []);
 
   return (
-    <div className="z-50 fixed top-0 left-0 w-screen h-screen bg-opaque">
-      <div className="h-screen lg:block flex flex-col justify-center items-center">
+    <div className="z-50 fixed top-0 left-0 w-screen h-screen bg-opaque ">
+      <div
+        className={`h-screen lg:block flex flex-col ${
+          imgDimensions &&
+          (imgDimensions.width > imgDimensions.height
+            ? "justify-center"
+            : "justify-start")
+        } md:justify-center items-center`}
+      >
         <FiX
-          className=" cursor-pointer lg:mt-16 lg:mx-64 text-white"
+          className="hidden md:block md:mb-8 cursor-pointer lg:mt-16 lg:mx-64 text-white"
           size={30}
           onClick={() => {
             setCopied(false);
@@ -61,7 +68,14 @@ const FullscreenImage = ({ event, link, smallerLink, toClose, imageIndex }) => {
             />
           </div>
           <img
-            className="max-w-full max-h-fit lg:h-auto mt-16 lg:mt-0 "
+            onLoad={(e) =>
+              setImgDimensions({
+                width: e.target.width,
+                height: e.target.height,
+              })
+            }
+            ref={imgRef}
+            className={`max-w-full max-h-fit md:h-auto mt-0 md:mt-0`}
             src={event.images[viewingImageIndex].smaller}
           ></img>
           <div className="hidden lg:flex h-full flex-col ml-4 justify-center">
@@ -80,7 +94,39 @@ const FullscreenImage = ({ event, link, smallerLink, toClose, imageIndex }) => {
           </div>
         </div>
 
-        <div className="justify-center flex items-center mt-4 ">
+        <div className="justify-center flex items-center mt-[2px] lg:mt-4">
+          {/* <FiChevronLeft
+            onClick={() => {
+              if (viewingImageIndex - 1 >= 0)
+                setViewingImageIndex(viewingImageIndex - 1);
+            }}
+            size={30}
+            className={`lg:hidden mr-6 ${
+              viewingImageIndex !== 0
+                ? "text-white cursor-pointer"
+                : "text-gray"
+            } `}
+          /> */}
+          <h1 className="text-lg  text-center font-bold justify-center flex items-center text-white">
+            {event.title}{" "}
+            <span className=" text-base ml-2 opacity-50 font-normal">
+              | {parseDate(event.date)}{" "}
+            </span>
+          </h1>
+          {/* <FiChevronRight
+            onClick={() => {
+              if (viewingImageIndex + 1 < event.images.length)
+                setViewingImageIndex(viewingImageIndex + 1);
+            }}
+            size={30}
+            className={`lg:hidden ml-6 ${
+              viewingImageIndex !== event.images.length - 1
+                ? "text-white cursor-pointer"
+                : "text-gray"
+            } `}
+          /> */}
+        </div>
+        <div className=" mt-[2px] lg:mt-4 mb-[2px] lg:mb-4 flex items-center  text-white justify-center">
           <FiChevronLeft
             onClick={() => {
               if (viewingImageIndex - 1 >= 0)
@@ -93,26 +139,6 @@ const FullscreenImage = ({ event, link, smallerLink, toClose, imageIndex }) => {
                 : "text-gray"
             } `}
           />
-          <h1 className="text-lg  text-center font-bold justify-center flex items-center text-white">
-            {event.title}{" "}
-            <span className=" text-base ml-2 opacity-50 font-normal">
-              | {parseDate(event.date)}{" "}
-            </span>
-          </h1>
-          <FiChevronRight
-            onClick={() => {
-              if (viewingImageIndex + 1 < event.images.length)
-                setViewingImageIndex(viewingImageIndex + 1);
-            }}
-            size={30}
-            className={`lg:hidden ml-6 ${
-              viewingImageIndex !== event.images.length - 1
-                ? "text-white cursor-pointer"
-                : "text-gray"
-            } `}
-          />
-        </div>
-        <div className="mt-4 flex lg:h-64 text-white justify-center">
           {copied ? (
             <FiCheck size={20} />
           ) : (
@@ -128,7 +154,27 @@ const FullscreenImage = ({ event, link, smallerLink, toClose, imageIndex }) => {
           <a href={link} download={link}>
             <FiDownload className="cursor-pointer ml-4" size={20} />
           </a>
+          <FiChevronRight
+            onClick={() => {
+              if (viewingImageIndex + 1 < event.images.length)
+                setViewingImageIndex(viewingImageIndex + 1);
+            }}
+            size={30}
+            className={`lg:hidden ml-6 ${
+              viewingImageIndex !== event.images.length - 1
+                ? "text-white cursor-pointer"
+                : "text-gray"
+            } `}
+          />
         </div>
+        <FiX
+          className="md:hidden cursor-pointer lg:mt-16 lg:mx-64 text-white"
+          size={30}
+          onClick={() => {
+            setCopied(false);
+            toClose();
+          }}
+        />
       </div>
     </div>
   );
